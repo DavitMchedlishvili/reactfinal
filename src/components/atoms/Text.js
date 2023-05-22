a67ea6126e67801a68eb43c4a1c8bf04ba0cee89
DavitMchedlishvili <118121651+DavitMchedlishvili@users.noreply.github.com>
2023-05-22 18:33:35 +0400

    add text and input atoms

---
+++ src/components/atoms/Text.js
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const Text = ({ children, variant = "body1" }) => {
+  return <Typography variant={variant}>{children}</Typography>;
+};
